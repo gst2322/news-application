120f2d723440feb4b53891aff27150420edd4a63
saiteja govoor <gsaitejakp@gmail.com>
2020-07-13 13:43:15 +0530

    deploy

--- src/app/newsmodal/newsmodal.component.ts
+++ src/app/newsmodal/newsmodal.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class NewsmodalComponent implements OnInit {
   @Input() title: string;
+  @Input() publishedAt: string;
+  @Input() img: string;
+  @Input() url: string;
+  @Input() content: string;
+  @Input() description: string;
+
   constructor() { }
 
   ngOnInit() {
